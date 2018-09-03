c8b130edc10383fb62a479fa09d9438b9827f428
xuexionghui <413669152@qq.com>
2018-09-03 16:16:14 +0800

    修改过后的springboot的启动类
    Signed-off-by: xuexionghui <413669152@qq.com>

--- src/main/java/com/itheima/Application.java
+++ src/main/java/com/itheima/Application.java
@@ -4,14 +4,21 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 /**
- *
+ *这里修改一下
  */
 @SpringBootApplication
 @MapperScan("com.itheima.mapper")   //扫描dao层接口所在的位置
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		//SpringApplication.run(Application.class, args);
+		SpringApplication application = new SpringApplication(Application.class);
+		application.setBannerMode(org.springframework.boot.Banner.Mode.OFF);
+		application.run(args);  //启动项目
+		
 	}
 }
